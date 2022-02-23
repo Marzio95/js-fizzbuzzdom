5b453282134c06a041c2dee7398eeea8d3c27b57
Marzio95 <marziodellarocca14@gmail.com>
2022-02-23 19:18:39 +0100

    rendo più chiaro js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 const containerBox = document.querySelector(".container")
 
+
 for (let i = 1; i <= 100; i++) {
     
     console.log(i);
 
     if (i % 3 == 0 && i % 5 == 0) {
-        element = `<div class="box fizz_buzz">FizzBuzz</div>`;
+        let element = `<div class="box fizz_buzz">FizzBuzz</div>`;
         console.log(element);
         containerBox.innerHTML += element;
+
     } else if (i % 3 == 0) {
-        element = `<div class="box fizz">Fizz</div>`;
+        let element = `<div class="box fizz">Fizz</div>`;
         console.log(element);
         containerBox.innerHTML += element;
     } else if (i % 5 == 0) {
-        element = `<div class="box buzz">Buzz</div>`;
+        let element = `<div class="box buzz">Buzz</div>`;
         containerBox.innerHTML += element;
         console.log(element);
     } else {
