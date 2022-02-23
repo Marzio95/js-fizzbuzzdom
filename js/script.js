97ab7ab732f31f6e1612b7393bf88ba15f7a0d36
Marzio95 <marziodellarocca14@gmail.com>
2022-02-23 17:03:44 +0100

    eseguo la funzione if con le condizioni del *3 *5 *15

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 const containerBox = document.querySelector(".container")
 
-for (let i = 1; i < 101; i++) {
+for (let i = 1; i <= 100; i++) {
     let element = `<div class="box">${i}</div>`;
     containerBox.innerHTML += element; 
-   
+    console.log(i);
+    
+   if (i % 3 == 0 && i % 5 == 0) {
+    let element = `<div class="box">FizzBuzz</div>`;   
+    containerBox.innerHTML += element;
+   }else if (i % 3 == 0) {
+        element = `<div class="box">Fizz</div>`
+        containerBox.innerHTML += element;
+    } else if (i % 5 == 0) {
+        let element = `<div class="box">Buzz</div>`;   
+        containerBox.innerHTML += element;        
+    } else {
+        let element = `<div class="box">${i}</div>`;
+        containerBox.innerHTML += element;
+    }
+
+
 }
