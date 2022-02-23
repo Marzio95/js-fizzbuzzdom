c642fc4000dff963d807b116d1c3e7a2a4e82370
Marzio95 <marziodellarocca14@gmail.com>
2022-02-23 18:03:32 +0100

    formatto il js

--- js/script.js
+++ js/script.js
@@ -2,20 +2,20 @@ const containerBox = document.querySelector(".container")
 
 for (let i = 1; i <= 100; i++) {
     let element = `<div class="box">${i}</div>`;
-    containerBox.innerHTML += element; 
-    console.log(i);
-    
-   if (i % 3 == 0 && i % 5 == 0) {
-    let element = `<div class="box fizz_buzz">FizzBuzz</div>`;   
     containerBox.innerHTML += element;
-   }else if (i % 3 == 0) {
+    console.log(i);
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        element = `<div class="box fizz_buzz">FizzBuzz</div>`;
+        containerBox.innerHTML += element;
+    } else if (i % 3 == 0) {
         element = `<div class="box fizz">Fizz</div>`
         containerBox.innerHTML += element;
     } else if (i % 5 == 0) {
-        let element = `<div class="box buzz">Buzz</div>`;   
-        containerBox.innerHTML += element;        
+        element = `<div class="box buzz">Buzz</div>`;
+        containerBox.innerHTML += element;
     } else {
-        
+
     }
 
 
